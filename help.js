c841161270e5050f503ae8e48fe3acc2cef4c44b
Ayushguptahack <47893590+Ayushguptahack@users.noreply.github.com>
2022-02-06 19:45:38 +0530

    help command addition

---
+++ help.js
@@ -0,0 +1,5 @@
+function help(){
+    console.log("Followig commands are available: \n 1. tree Path_Name or -t Path_Name (IF PATH NOT GIVEN IT WILL WORK ON CURRENT WROKING DIRECTORY)\n 2. help or -h \n3. organize Path_Name or -o Path_Name(IF PATH NOT GIVEN IT WILL WORK ON CURRENT WROKING DIRECTORY)")
+}
+
+module.exports = help;
--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 let fs = require('fs');
-let cmd_type = process.argv[2];
+
+
+if(process.argv[2] === undefined){
+    console.log("Sorry no command with this name please check the syntax.");
+    return;
+}
+
+let cmd_type = process.argv[2].toLowerCase();
 let path = process.argv[3];
+
 let tree = require('./tree.js');
 
-if(cmd_type === "tree"){
+
+if(cmd_type === "--tree" || cmd_type === "-t"){
     if(fs.existsSync(path) && fs.lstatSync(path).isDirectory()){
         tree.fun(path);
+    }else if(path === undefined){
+        tree.fun(process.cwd());
     }else{
         console.log("INVALID PATH");
     }
-}else{
-    console.log("sorry no command with this name");
+}else if(cmd_type === "--help" || cmd_type === "-h"){
+    console.log("Followig commands are available: \n 1. tree Path_Name or -t Path_Name (IF PATH NOT GIVEN IT WILL WORK ON CURRENT WROKING DIRECTORY)\n 2. help or -h \n3. organize Path_Name or -o Path_Name(IF PATH NOT GIVEN IT WILL WORK ON CURRENT WROKING DIRECTORY)")
+}
+else{
+    console.log("Sorry no command with this name please check the syntax.");
 }
 
 
